ce58e6f2429c73a008f8e320def71a7337edc691
amirasayedsailfishrov-gif <amira.sayed.sailfishrov@gmail.com>
2026-03-20 20:04:22 +0200

    Fix settings validation: ignore extra env vars

--- app/core/config.py
+++ app/core/config.py
@@ -20,7 +20,12 @@ class Settings(BaseSettings):
         alias="CORS_ALLOWED_ORIGINS",
     )
 
-    model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8", case_sensitive=False)
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        env_file_encoding="utf-8",
+        case_sensitive=False,
+        extra="ignore",
+    )
 
     @property
     def parsed_cors_origins(self) -> List[str]:
